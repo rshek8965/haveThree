aca943fe0a9ca8af1eea3dbc08d9b7dbee1b8232
rshek8965 <rshek8965@bths.edu>
2018-11-29 11:32:39 -0500

    edited in method & print response

--- src/com/company/haveThree.java
+++ src/com/company/haveThree.java
@@ -1,9 +1,22 @@
 package com.company;
 
 public class haveThree {
-    public static check(int[] arr)
+    public static boolean check(int[] arr)
     {
-        
-
+        int count=1;
+        for (int i = 0;i<arr.length-1; i++)
+        {
+            if (arr[i]==3)
+            {
+                count++;
+                if (arr[i+1]==3)
+                {
+                    return false;
+                }
+            }
+        }
+        if (count==3)
+        {return true;}
+        else return false;
     }
 }
--- src/com/company/runner.java
+++ src/com/company/runner.java
@@ -7,6 +7,8 @@ public class runner {
 	int[] arr2 = {3, 1, 3, 3}; //false
 	int[] arr3 = {3, 4, 3, 3, 4}; //false
 
-
+        System.out.println(haveThree.check(arr));
+        System.out.println(haveThree.check(arr2));
+        System.out.println(haveThree.check(arr3));
     }
 }
